74d59e87d450ff33c8e6bdaf0776b5d6dc525b1d
Guest <guest-euobkz@h26100.sgb.skir.kr>
2020-12-12 23:50:13 +0900

    실패한 색깔입히기 시도

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+const range = document.getElementById("jsRange");
+
+const colors = document.getElementsByClassName("controls__color");
+
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
-ctx.strokestyle = "#2c2c2c";
+ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -29,9 +33,19 @@ function onMouseMove(event) {
     }
 }
 
+function changeStrokeStyle(event) {
+    ctx.strokeStyle = "#800080";
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+}
+
+if(colors) {
+    for(let i=0; i< colors.length; i++) {
+        colors[i].addEventListener("onclick", changeStrokeStyle);
+    }
 }
